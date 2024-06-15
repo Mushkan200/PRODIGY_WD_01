4b00d7439d687f979e27a72c699506417fddaab6
Mushkan200 <nitika.sharmaa2019@gmail.com>
2024-06-15 11:04:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add event listener to submit button
+document.getElementById("submit-btn").addEventListener("click", function() {
+    // Perform some action when the button is clicked
+    console.log("Button clicked!");
+});
+
+// Add event listener to subscribe button
+document.getElementById("subscribe-btn").addEventListener("click", function() {
+    // Perform some action when the button is clicked
+    console.log("Subscribe button clicked!");
+});
